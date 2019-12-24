228c1aa6e0c3b08b821bf52fb9e57e4aaf689775
Alexander Römer <alex@roemer.design>
2019-12-24 10:09:32 +0100

    Initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var calculatorLabel: UILabel!
+    
+    private var isTypingNumber = false
+    private var firstNumber    = 0
+    private var secondNumber   = 0
+    private var operation      = "  "
+    
+    @IBAction private func numberTapped(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        
+        if isTypingNumber {
+            calculatorLabel.text = calculatorLabel.text! + number
+        } else {
+            calculatorLabel.text = number
+            isTypingNumber = true
+        }
     }
-
-
+    
+    @IBAction private func calculationTapped(_ sender: UIButton) {
+        isTypingNumber = false
+        firstNumber    = Int(calculatorLabel.text!)!
+        operation      = sender.currentTitle!
+    }
+    
+    
+    @IBAction private func restart(_ sender: UIButton) {
+        calculatorLabel.text = "0"
+        firstNumber = 0
+        secondNumber = 0
+    }
+    
+    @IBAction private func equalsTapped(_ sender: UIButton) {
+        isTypingNumber = false
+        var result     = 0
+        secondNumber   = Int(calculatorLabel.text!)!
+        
+        if operation == "+" {
+            result = firstNumber + secondNumber
+        } else if operation == "-" {
+            result = firstNumber - secondNumber
+        } else if operation == "x" {
+            result = firstNumber * secondNumber
+        } else if operation == "÷" {
+            result = firstNumber / secondNumber
+        }
+        calculatorLabel.text = String(result)
+    }
+    
 }
 
